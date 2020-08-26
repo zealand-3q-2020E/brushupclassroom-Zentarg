b61bc9f7933278e206e78dc9209987dcf8c2aba6
Zentarg <apg.chili@gmail.com>
2020-08-26 12:32:39 +0200

    Adds student class

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            BirthDay = birthDay;
+        }
+
+        public string Name { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+
+    }
+}
